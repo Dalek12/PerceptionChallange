9fbecfe1490c772f9b92e3310e32baba96eccf5a
Dalek12 <62363657+Dalek12@users.noreply.github.com>
2023-10-06 21:50:42 -0500

    Update boundary_detection.py

--- boundary_detection.py
+++ boundary_detection.py
@@ -12,43 +12,63 @@ def fit_line_through_centroids(centroids, img_height):
     bottom_x = int(slope * img_height + intercept)
     return (top_x, 0), (bottom_x, img_height)
 
+def extract_centroids(contours):
+    "Extract centroids for given contours."
+    centroids = []
+    for contour in contours:
+        M = cv2.moments(contour)
+        if M['m00'] != 0:
+            cx = int(M['m10'] / M['m00'])
+            cy = int(M['m01'] / M['m00'])
+            centroids.append((cx, cy))
+    return centroids
+
+def improved_filter_contours(contours, img_shape):
+    "Improved contour filtering based on area, aspect ratio, and position."
+    filtered = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        x, y, w, h = cv2.boundingRect(contour)
+        if x + w > 0.7 * img_shape[1] and y < 0.2 * img_shape[0]:
+            continue
+        if area > 100:
+            aspect_ratio = float(w) / h
+            if 0.5 < aspect_ratio < 2:
+                filtered.append(contour)
+    return filtered
+
 # Load the image
 img_path = 'red.png'
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-# Filter for red regions
+# Filter for red regions and find contours
 lower_red = np.array([0, 0, 100])
 upper_red = np.array([70, 70, 255])
 mask = cv2.inRange(img, lower_red, upper_red)
 res = cv2.bitwise_and(img, img, mask=mask)
-
-# Convert to grayscale and threshold
 gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-# Find contours
 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-filtered_contours = [contour for contour in contours if cv2.contourArea(contour) > 100]
 
-# Extract centroids
-centroid_points = [(int(M['m10'] / M['m00']) if M['m00'] != 0 else 0, int(M['m01'] / M['m00']) if M['m00'] != 0 else 0) for M in [cv2.moments(c) for c in filtered_contours]]
+# Improved contour filtering and centroid extraction
+filtered_contours = improved_filter_contours(contours, img_rgb.shape)
+centroids = extract_centroids(filtered_contours)
 
-# Provided centroids for left and right cones
-left_cone_centroids = [centroid_points[i] for i in [1, 3, 5, 7, 9, 11, 13]]
-right_cone_centroids = [centroid_points[i] for i in [0, 2, 4, 6, 8, 10, 12]]
+# Sort centroids and determine left and right cones
+sorted_centroids = sorted(centroids, key=lambda x: x[0])
+mid_index = len(sorted_centroids) // 2
+left_cone_centroids = sorted_centroids[:mid_index]
+right_cone_centroids = sorted_centroids[mid_index:]
 
 # Fit lines through the centroids
 left_line_start, left_line_end = fit_line_through_centroids(left_cone_centroids, img.shape[0])
 right_line_start, right_line_end = fit_line_through_centroids(right_cone_centroids, img.shape[0])
 
-# Draw lines on the image
-img_with_final_boundaries = img_rgb.copy()
-cv2.line(img_with_final_boundaries, left_line_start, left_line_end, (255, 0, 0), 2)
-cv2.line(img_with_final_boundaries, right_line_start, right_line_end, (255, 0, 0), 2)
-
-# Save the image
+# Draw the boundary lines on the image
+cv2.line(img_rgb, left_line_start, left_line_end, (255, 0, 0), 2)
+cv2.line(img_rgb, right_line_start, right_line_end, (255, 0, 0), 2)
 output_path = 'answer.png'
-cv2.imwrite(output_path, cv2.cvtColor(img_with_final_boundaries, cv2.COLOR_RGB2BGR))
+cv2.imwrite(output_path, cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR))
 
 
